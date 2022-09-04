1c79647cee8243c9fbb077b33415b90ef275d076
Porotikov Sergey <OrionChrome@yandex.ru>
2022-09-04 23:31:21 +0300

    feat: простые проблемы решены. Впереди более сложные

--- src/main/java/ru/practicum/shareit/requests/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/requests/ItemRequestDto.java
@@ -1,4 +1,10 @@
 package ru.practicum.shareit.requests;
 
+import java.time.LocalDateTime;
+
 public class ItemRequestDto {
+    private long id;
+    private String description;
+    private long requestor;
+    private LocalDateTime createDate;
 }
