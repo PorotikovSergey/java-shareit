a01116a87f718e37cadbe42d706700045b554059
Porotikov Sergey <OrionChrome@yandex.ru>
2022-09-23 23:22:57 +0300

    feat: исправлен ErrorHandler

--- src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -201,3 +201,5 @@ public class BookingServiceImpl implements BookingService {
         return result;
     }
 }
+
+
--- src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -34,7 +35,15 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)  //500
-    public ErrorResponse handleThrowable(final ServiceException e) {
+    public ErrorResponse handleThrowable(final Throwable e) {
+        return new ErrorResponse(
+                e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)  //400
+    public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
         return new ErrorResponse(
                 e.getMessage()
         );
